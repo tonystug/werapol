c375cbfc5cff8cf4f584b99635eb6f15c6639585
tonystug <werapol@srithatpit.ac.th>
2025-01-17 11:53:45 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+    <p>-----การแสดงข้อความแบบหัวข้อ------</p>
+    <div>
+        <h1>HTMLเบื้องต้น(h1)</h1>
+    </div>
+    <div>
+        <h2>HTMLเบื้องต้น(h2)</h2>
+        <h3>HTMLเบื้องต้น(h2)</h3>
+        <h4>HTMLเบื้องต้น(h2)</h4>
+        <h5>HTMLเบื้องต้น(h2)</h5>
+        <h6>HTMLเบื้องต้น(h2)</h6>
+    </div>
+    <p>-----การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+    <div>
+        <b>วีระพล ศรีมูลดา</b><br>
+        <i>วีระพล ศรีมูลดา</i><br>
+        <u>วีระพล ศรีมูลดา</u><br>
+    </div>
+    <p><b>-----การ Link------</b></p>
+    <div>
+        <!--Linkหน้าเดียว-->
+        <a href = "https://www.google.co.th">Google</a><br>
+    </div>
+
+</body>
+</html>
